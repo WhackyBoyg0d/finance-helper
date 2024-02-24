37bad6bd57cbe01a495b0bb0294931ad564dc5ec
firesmasher231 <66473811+firesmasher231@users.noreply.github.com>
2024-02-24 09:48:34 +0000

    tailwind install

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import './App.css';
-import Home from './components/Home';
-import Profile from './components/Profile';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
 
 function App(): JSX.Element {
-  return (
-    <BrowserRouter>
-		  <Routes>
-			  <Route path="/" element={<Home />} />
-			  <Route path="/profile" element={<Profile />} />
-		  </Routes>
-	</BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Login(): JSX.Element {
+	return <p className="bg-red-500">Login</p>;
+}
+
+export default Login;
