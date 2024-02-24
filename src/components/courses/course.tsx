1b12bd6fb4136467ded5a0f3d58545b29ae71463
baileyluTCD <baileylu@tcd.ie>
2024-02-24 12:54:16 +0000

    finished course type

--- src/components/courses/course.tsx
+++ src/components/courses/course.tsx
@@ -1,3 +1,17 @@
 interface Course {
     name: string;
+    slides: Array<Slide>;
+    questions: Array<MultiChoiceQuestion>;
 }
+
+interface Slide {
+    title: string;
+    body: Array<string>;
+    image_source: string;
+}
+
+interface MultiChoiceQuestion {
+    question: string;
+    answer: string
+}
+
