30a1c7d1e9444fdd375aeeff08bb551363229cbc
firesmasher231 <asjosh7@gmail.com>
2024-02-24 13:44:30 +0000

    Onboarding

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 export function Login(): JSX.Element {
-	return <p className="bg-red-500">Login</p>;
+	return (
+	<div className="flex justify-center align-middle ">
+		<div className="flex flex-col">
+			<div className="flex justify-center align-middle">
+				<button className=" text-white p-2 m-2"></button>
+				<button className=" text-white p-2 m-2">Twitter</button>
+				<button className=" text-white p-2 m-2">Facebook</button>
+			</div>
+			<input type="text" placeholder="Username" className="border-2 border-gray-300 p-2 m-2" />
+			<input type="password" placeholder="Password" className="border-2 border-gray-300 p-2 m-2" />
+			<button className="bg-blue-500 text-white p-2 m-2">Login</button>
+		</div>
+	</div>
+		);
 }
 
 export default Login;
