0e2f2bf415be58efe010420b56f49f8e4203240e
baileyluTCD <baileylu@tcd.ie>
2024-02-25 11:49:22 +0000

    fixed bad persistence value

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword, setPersistence } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database'; 
 
@@ -15,7 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-setPersistence(auth, {type: 'LOCAL'});
+setPersistence(auth, browserLocalPersistence);
 const firestore = getFirestore(app);
 const database = getDatabase(app);
 
